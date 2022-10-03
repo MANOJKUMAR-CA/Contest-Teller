1d1b7fa4237157988c05729bf6491e22c5317db0
gunzeux <gunzeux71@gmail.com>
2022-10-03 19:26:58 +0530

    Added JSON module and a webAddr

--- Source Main.py
+++ Source Main.py
@@ -4,13 +4,16 @@ Created on Sun Oct  2 20:21:39 2022
 
 @author: Bala
 """
-
+import json
 from bs4 import BeautifulSoup
 import requests
 
-URL = {"test":"https://webscraper.io/test-sites/e-commerce/allinone", 1:"http://www.collegeevents.info/search-events"};
+URL = {"test":"https://webscraper.io/test-sites/e-commerce/allinone", "Event":"http://www.collegeevents.info/search-events", "Hack":"https://www.eventbrite.com/d/online/science-and-tech--events/hackathon/?page=1"};
 
-page = requests.get(URL[1])
+page = requests.get(URL["Hack"])
 soup = BeautifulSoup(page.content,"html.parser")
 
-print(soup)
+data = json.loads(soup.find('script', type='application/ld+json').text)
+
+for i in data:
+    print(i['name'])
