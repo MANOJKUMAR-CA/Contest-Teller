4b471e33cae94eb019edb607a0064197661c9dd8
DIVYESH-SENTRY <sentry695@gmail.com>
2022-11-13 08:34:41 +0530

    Added Try and Catch Block

--- Source Main.py
+++ Source Main.py
@@ -5,7 +5,6 @@ Created on Sun Oct  2 20:21:39 2022
 @author: Bala
 """
 import json
-
 import openpyxl
 from bs4 import BeautifulSoup
 import requests
@@ -20,7 +19,7 @@ sheet.append(['Name','SD','ED','LOCATION'])'''
 def printData(data):
     for i in data:
         strtext = i["name"] + "|| SD: " + i['startDate'] +"|| ED:"+i['endDate']+"|| LOC: " +i['location']['@type']
-        print(strtext)
+        print("[+]"+strtext)
         #sheet.append([i['name'],i['startDate'],i['endDate'],i['location']['@type']])
         for i in range(0,len(strtext)):
             print("*",end="")
@@ -37,14 +36,20 @@ soup = BeautifulSoup(page.text,"html.parser")
 lastpage=soup.find('li').find('span',class_="eds-text-color--control eds-text-weight--heavy").text
 print(lastpage) '''
 
-
+res = 0
 lastpage=228
-for i in range(1,lastpage):
-    furl=url+str(i)
-    page = requests.get(furl)
-    soup = BeautifulSoup(page.text,"html.parser")
-    data = json.loads(soup.find('script', type='application/ld+json').text)
-    printData(data)
+try:
+    for i in range(1,lastpage):
+        furl=url+str(i)
+        page = requests.get(furl)
+        soup = BeautifulSoup(page.text,"html.parser")
+        data = json.loads(soup.find('script', type='application/ld+json').text)
+        printData(data)
+        res+=1
+except:
+    print("[-]Error occured.. terminating")
+finally:
+    print("[+]Total results: "+ str(res) +"\n[+]Closing application")
 
 
 #excel.save("Project.xlsx")
